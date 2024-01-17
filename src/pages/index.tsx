82d043d213fc43e390db036d3fc6967026624bb7
mitkapanarin <mitka_j@yahoo.com>
2024-01-17 22:46:08 +0100

    pages added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
 import Signup from "./Signup";
 import Login from "./Login";
 import ErrorPage from "./ErrorPage";
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
+import Cart from "./Cart";
+import Profile from "./Profile";
+import Events from "./Events";
+import EventDetailsPage from "./EventDetailsPage";
+import Checkout from "./Checkout";
 
-export { Signup, Login, ErrorPage };
+export {
+  Signup,
+  Login,
+  ErrorPage,
+  EventDetailsPage,
+  Events,
+  ForgotPassword,
+  Cart,
+  Profile,
+  ResetPassword,
+  Checkout,
+};
