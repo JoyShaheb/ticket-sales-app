af8d99f307a9c81726115620ad392b7bfc558121
Joy Shaheb <khondokoralam@gmail.com>
2024-02-10 01:25:31 +0600

    returned callbacks from sent emails

--- src/store/API/userAuthAPI.ts
+++ src/store/API/userAuthAPI.ts
@@ -99,10 +99,7 @@ export const UserAuthAPI = createApi({
     sendEmailVerification: builder.mutation<string, null>({
       queryFn: async () => {
         try {
-          await sendEmailVerification(auth.currentUser as User, {
-            // redirect to this page only after request is successful
-            url: "http://localhost:5173/",
-          });
+          await sendEmailVerification(auth.currentUser as User);
           return {
             data: "Email verification sent to your email",
           };
@@ -152,10 +149,7 @@ export const UserAuthAPI = createApi({
     >({
       queryFn: async ({ email }) => {
         try {
-          await sendPasswordResetEmail(auth, email, {
-            // redirect to this page only after request is successful
-            url: "http://localhost:5173/",
-          });
+          await sendPasswordResetEmail(auth, email);
           return {
             data: "Password reset link sent to your email",
           };
