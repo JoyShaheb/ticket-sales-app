1970f2dc2fb1de0d661d9d9d495a1d64d6ca99bd
Joy Shaheb <khondokoralam@gmail.com>
2024-02-20 19:12:17 +0600

    ...

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,7 +54,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
+        /* @eslint-disable-next-line no-unused-vars */
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        /* @eslint-disable-next-line no-unused-vars */
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
